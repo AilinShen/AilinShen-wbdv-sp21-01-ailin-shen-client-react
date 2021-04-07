fa24025bfb9740a703d351f4f955b8764282179d
AilinShen <aln67911@gmail.com>
2021-04-07 13:21:31 -0400

    styling color change

--- src/components/quizzes/questions/multiple-choice-question.js
+++ src/components/quizzes/questions/multiple-choice-question.js
@@ -1,25 +1,77 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 function MultipleChoiceQuestion({question}){
+
+    const choiceLength = question.choices.length
+    const [answer, setAnswer] = useState("")
+    const [graded, setGraded] = useState(false)
+
     return(
         <div className="mt-3">
-            <h4>{question.question}</h4>
-            <ul className="list-group">
+            <h4 className="ml-3">
+                {question.question}
                 {
-                    question.choices.map((choice) => {
+                    graded &&
+                    (answer === question.correct) &&
+                    <span className="ml-3" style={{color: "green"}}>
+                        <i className="fas fa-check"></i>
+                    </span>
+                }
+                {
+                    graded &&
+                    (answer !== question.correct) &&
+                    <span className="ml-3" style={{color: "red"}}>
+                        <i className="fas fa-times"></i>
+                    </span>
+                }
+
+            </h4>
+            <ul className="list-group">
+
+                {
+                    question.choices.map((choice, i) => {
                         return(
-                            <li className={`list-group-item`}>
+                            <li className={`list-group-item w-50 
+                            ${ (graded && (question.correct === choice)) ? "background-green":"" }
+                            ${ (graded && (question.correct !== choice) && (answer === choice)) ? "background-red":"" }`
+                            }>
                                 <label>
                                     <input type="radio"
+                                           onClick={() => {
+                                               setGraded(false)
+                                               setAnswer(choice)
+                                           }}
                                            name={question._id}/>
                                     {choice}
                                 </label>
+                                {
+                                    (graded && (question.correct === choice)) &&
+                                    <span className="ml-3" style={{color: "green"}}>
+                                        <i className="fas fa-check float-right"></i>
+                                    </span>
+                                }
+
+                                {
+                                    (graded && (question.correct !== choice) && (answer === choice)) &&
+                                    <span className="ml-3" style={{color: "red"}}>
+                                        <i className="fas fa-times float-right"></i>
+                                    </span>
+                                }
+
                             </li>
                         )
                     })
                 }
+
             </ul>
+
+            <h6 className="m-4">Your answer: { (answer!== "") && answer}</h6>
+            <button type="button" className="btn btn-success ml-4 mb-3"
+                    onClick={()=>{setGraded(true)}}>
+                Grade
+            </button>
+
         </div>
     )
 }
--- src/components/quizzes/questions/true-false-question.js
+++ src/components/quizzes/questions/true-false-question.js
@@ -1,15 +1,100 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function TrueFalseQuestion({question}){
+    const [answer, setAnswer] = useState("")
+    const [graded, setGraded] = useState(false)
+
     return(
         <div className="mt-3">
-            <h4>{question.question}</h4>
-            <ul>
-                <input type="radio" id="male" name="gender" value="male"/>
-                <label htmlFor="male">Male</label><br/>
-                <input type="radio" id="male" name="gender" value="male"/>
-                <label htmlFor="male">Male</label><br/>
+            <h4 className="ml-3">
+                {question.question}
+                {
+                    graded &&
+                    (answer === question.correct) &&
+                    <span className="ml-3" style={{color: "green"}}>
+                        <i className="fas fa-check"></i>
+                    </span>
+                }
+                {
+                    graded &&
+                    (answer !== question.correct) &&
+                    <span className="ml-3" style={{color: "red"}}>
+                        <i className="fas fa-times"></i>
+                    </span>
+                }
+
+            </h4>
+
+
+            <ul className="list-group">
+                <li className={`list-group-item w-50 
+                ${ (graded && (question.correct === "true")) ? "background-green":"" }
+                ${ (graded && (question.correct !== "true") && (answer === "true")) ? "background-red":"" }`}>
+                    <label>
+                        <input
+                            type="radio"
+                            onClick={() => {
+                                setGraded(false)
+                                setAnswer("true")
+                            }}
+                            name={question._id}/>
+                        TRUE
+                    </label>
+
+                    {
+                        (graded && (question.correct === "true")) &&
+                        <span className="ml-3" style={{color: "green"}}>
+                            <i className="fas fa-check float-right"></i>
+                        </span>
+                    }
+
+                    {
+                        (graded && (question.correct !== "true") && (answer === "true")) &&
+                        <span className="ml-3" style={{color: "red"}}>
+                            <i className="fas fa-times float-right"></i>
+                        </span>
+                    }
+                </li>
+
+
+                <li className={`list-group-item w-50 
+                ${ (graded && (question.correct === "false")) ? "background-green":"" }
+                ${ (graded && (question.correct !== "false") && (answer === "false")) ? "background-red":"" }`}>
+                    <label>
+                        <input
+                            type="radio"
+                            onClick={() => {
+                                setGraded(false)
+                                setAnswer("false")
+                            }}
+                            name={question._id}/>
+                        FALSE
+                    </label>
+                    {
+                        (graded && (question.correct === "false")) &&
+                        <span className="ml-3" style={{color: "green"}}>
+                            <i className="fas fa-check float-right"></i>
+                        </span>
+                    }
+
+                    {
+                        (graded && (question.correct !== "false") && (answer === "false")) &&
+                        <span className="ml-3" style={{color: "red"}}>
+                            <i className="fas fa-times float-right"></i>
+                        </span>
+                    }
+                </li>
+
             </ul>
+
+
+            <h6 className="m-4">Your answer: { (answer!== "") && answer}</h6>
+            <button type="button"
+                    className="btn btn-success ml-4 mb-3"
+                    onClick={()=>{setGraded(true)}}>
+                Grade
+            </button>
+
         </div>
     )
 }
